be85c0fd12cb8e43fb61c9257d4cb5becd75e511
Luckson Vilus <vilusl@gmail.com>
2019-07-22 19:50:48 -0400

    adding the client module artefact

---
+++ client/src/App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import OrtherPage from './OtherPage';
+import Fib from './Fib'
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+          <Link className="App-link" to="/">Home</Link>
+          <Link className="App-link" to="/otherpage">Other Page</Link>
+        </header>
+        <div>
+          <Route exact path="/" component={Fib} />
+          <Route path="/otherpage" component={OrtherPage} />
+        </div>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
